2a9d9a4580c1b78c7c5818760b5b94e535a0ac91
hsicen <codinghuang@163.com>
2021-05-19 11:19:12 +0800

    修改工具类的可见范围

--- magicindicator/src/main/java/com/hsicen/magicindicator/indicators/DrawableIndicator.kt
+++ magicindicator/src/main/java/com/hsicen/magicindicator/indicators/DrawableIndicator.kt
@@ -1,17 +1,18 @@
 package com.hsicen.magicindicator.indicators
 
 import android.content.Context
+import android.content.res.Resources
 import android.graphics.Canvas
 import android.graphics.RectF
 import android.graphics.drawable.Drawable
+import android.util.TypedValue
 import android.view.View
 import android.view.animation.LinearInterpolator
 import androidx.annotation.DrawableRes
+import androidx.core.content.ContextCompat
 import com.hsicen.magicindicator.FragmentContainerHelper
 import com.hsicen.magicindicator.R
 import com.hsicen.magicindicator.abs.IPagerIndicator
-import com.hsicen.magicindicator.dp2px
-import com.hsicen.magicindicator.drawableRes
 import com.hsicen.magicindicator.model.PositionData
 
 /**
@@ -21,73 +22,82 @@ import com.hsicen.magicindicator.model.PositionData
  * 描述：Drawable Indicator
  */
 class DrawableIndicator(context: Context) : View(context),
-    IPagerIndicator {
-    private var mPositionDataList: List<PositionData> = listOf()
-    private val mRect = RectF()
+  IPagerIndicator {
+  private var mPositionDataList: List<PositionData> = listOf()
+  private val mRect = RectF()
 
-    private var mLeftPad = 26.dp2px
-    private var mRightPad = 26.dp2px
-    private var mTopPad = 12.dp2px
-    private var mBottomPad = 19.dp2px
-    private var mDrawable = drawableRes(R.drawable.icon_type)
-    private var mStartInterpolator = LinearInterpolator()
-    private var mEndInterpolator = LinearInterpolator()
+  private var mLeftPad = 26.dp2px
+  private var mRightPad = 26.dp2px
+  private var mTopPad = 12.dp2px
+  private var mBottomPad = 19.dp2px
+  private var mDrawable = drawableRes(R.drawable.icon_type)
+  private var mStartInterpolator = LinearInterpolator()
+  private var mEndInterpolator = LinearInterpolator()
 
-    override fun onDraw(canvas: Canvas?) {
-        canvas ?: return
-        mDrawable?.setBounds(
-            mRect.left.toInt(),
-            mRect.top.toInt(),
-            mRect.right.toInt(),
-            mRect.bottom.toInt()
-        )
-        mDrawable?.draw(canvas)
-    }
+  override fun onDraw(canvas: Canvas?) {
+    canvas ?: return
+    mDrawable?.setBounds(
+      mRect.left.toInt(),
+      mRect.top.toInt(),
+      mRect.right.toInt(),
+      mRect.bottom.toInt()
+    )
+    mDrawable?.draw(canvas)
+  }
 
-    override fun onPageScrolled(position: Int, positionOffset: Float, positionOffsetPixels: Int) {
-        if (mPositionDataList.isEmpty()) return
+  override fun onPageScrolled(position: Int, positionOffset: Float, positionOffsetPixels: Int) {
+    if (mPositionDataList.isEmpty()) return
 
-        // 计算锚点位置
-        val current = FragmentContainerHelper.getImitativePositionData(mPositionDataList, position)
-        val next = FragmentContainerHelper.getImitativePositionData(mPositionDataList, position + 1)
+    // 计算锚点位置
+    val current = FragmentContainerHelper.getImitativePositionData(mPositionDataList, position)
+    val next = FragmentContainerHelper.getImitativePositionData(mPositionDataList, position + 1)
 
-        mRect.left = current.mContentLeft - mLeftPad +
-            (next.mContentLeft - current.mContentLeft) *
-            mEndInterpolator.getInterpolation(positionOffset)
-        mRect.top = (current.mContentTop - mTopPad).toFloat()
-        mRect.right = current.mContentRight + mRightPad +
-            (next.mContentRight - current.mContentRight) *
-            mStartInterpolator.getInterpolation(positionOffset)
-        mRect.bottom = (current.mContentBottom + mBottomPad).toFloat()
+    mRect.left = current.mContentLeft - mLeftPad +
+      (next.mContentLeft - current.mContentLeft) *
+      mEndInterpolator.getInterpolation(positionOffset)
+    mRect.top = (current.mContentTop - mTopPad).toFloat()
+    mRect.right = current.mContentRight + mRightPad +
+      (next.mContentRight - current.mContentRight) *
+      mStartInterpolator.getInterpolation(positionOffset)
+    mRect.bottom = (current.mContentBottom + mBottomPad).toFloat()
 
-        invalidate()
-    }
+    invalidate()
+  }
 
-    override fun onPositionDataProvide(dataList: MutableList<PositionData>?) {
-        dataList ?: return
-        mPositionDataList = dataList
-    }
+  override fun onPositionDataProvide(dataList: MutableList<PositionData>?) {
+    dataList ?: return
+    mPositionDataList = dataList
+  }
 
-    override fun onPageScrollStateChanged(state: Int) {}
-    override fun onPageSelected(position: Int) {}
+  override fun onPageScrollStateChanged(state: Int) {}
+  override fun onPageSelected(position: Int) {}
 
-    fun updatePadding(
-        left: Int = mLeftPad,
-        top: Int = mTopPad,
-        right: Int = mRightPad,
-        bottom: Int = mBottomPad
-    ) {
-        mLeftPad = left
-        mTopPad = top
-        mRightPad = right
-        mBottomPad = bottom
-    }
+  fun updatePadding(
+    left: Int = mLeftPad,
+    top: Int = mTopPad,
+    right: Int = mRightPad,
+    bottom: Int = mBottomPad
+  ) {
+    mLeftPad = left
+    mTopPad = top
+    mRightPad = right
+    mBottomPad = bottom
+  }
 
-    fun updateDrawable(@DrawableRes resId: Int) {
-        mDrawable = drawableRes(resId)
-    }
+  fun updateDrawable(@DrawableRes resId: Int) {
+    mDrawable = drawableRes(resId)
+  }
 
-    fun updateDrawable(drawable: Drawable) {
-        mDrawable = drawable
-    }
+  fun updateDrawable(drawable: Drawable) {
+    mDrawable = drawable
+  }
+
+  private fun Context.drawableRes(@DrawableRes resId: Int) = ContextCompat.getDrawable(this, resId)
+
+  private fun View.drawableRes(@DrawableRes resId: Int) = context.drawableRes(resId)
+
+  private val Int.dp2px: Int
+    get() = TypedValue.applyDimension(
+      TypedValue.COMPLEX_UNIT_DIP, this * 1.0f, Resources.getSystem().displayMetrics
+    ).toInt()
 }
--- magicindicator/src/main/java/com/hsicen/magicindicator/titles/SimplePagerTitleView.kt
+++ magicindicator/src/main/java/com/hsicen/magicindicator/titles/SimplePagerTitleView.kt
@@ -1,12 +1,13 @@
 package com.hsicen.magicindicator.titles
 
 import android.content.Context
+import android.content.res.Resources
 import android.graphics.Rect
 import android.text.TextUtils
+import android.util.TypedValue
 import android.view.Gravity
 import android.widget.TextView
 import com.hsicen.magicindicator.abs.IMeasurablePagerTitleView
-import com.hsicen.magicindicator.dp2px
 
 /**
  * 作者：hsicen  5/3/21 09:19
@@ -15,69 +16,74 @@ import com.hsicen.magicindicator.dp2px
  * 描述：文字标题
  */
 open class SimplePagerTitleView(context: Context) :
-    TextView(context, null),
-    IMeasurablePagerTitleView {
-    var selectedColor = 0
-    var normalColor = 0
+  TextView(context, null),
+  IMeasurablePagerTitleView {
+  var selectedColor = 0
+  var normalColor = 0
 
-    init {
-        isSingleLine = true
-        gravity = Gravity.CENTER
-        ellipsize = TextUtils.TruncateAt.END
-        setPadding(10.dp2px, 0, 10.dp2px, 0)
-    }
+  init {
+    isSingleLine = true
+    gravity = Gravity.CENTER
+    ellipsize = TextUtils.TruncateAt.END
+    setPadding(10.dp2px, 0, 10.dp2px, 0)
+  }
 
-    override fun onSelected(index: Int, totalCount: Int) {
-        setTextColor(selectedColor)
-    }
+  override fun onSelected(index: Int, totalCount: Int) {
+    setTextColor(selectedColor)
+  }
 
-    override fun onDeselected(index: Int, totalCount: Int) {
-        setTextColor(normalColor)
-    }
+  override fun onDeselected(index: Int, totalCount: Int) {
+    setTextColor(normalColor)
+  }
 
-    override fun onLeave(index: Int, totalCount: Int, leavePercent: Float, leftToRight: Boolean) {}
-    override fun onEnter(index: Int, totalCount: Int, enterPercent: Float, leftToRight: Boolean) {}
-    override fun getContentLeft(): Int {
-        val bound = Rect()
-        var longestString = ""
-        if (text.toString().contains("\n")) {
-            val brokenStrings = text.toString().split("\\n").toTypedArray()
-            for (each in brokenStrings) {
-                if (each.length > longestString.length) longestString = each
-            }
-        } else {
-            longestString = text.toString()
-        }
-        paint.getTextBounds(longestString, 0, longestString.length, bound)
-        val contentWidth = bound.width()
-        return left + width / 2 - contentWidth / 2
+  override fun onLeave(index: Int, totalCount: Int, leavePercent: Float, leftToRight: Boolean) {}
+  override fun onEnter(index: Int, totalCount: Int, enterPercent: Float, leftToRight: Boolean) {}
+  override fun getContentLeft(): Int {
+    val bound = Rect()
+    var longestString = ""
+    if (text.toString().contains("\n")) {
+      val brokenStrings = text.toString().split("\\n").toTypedArray()
+      for (each in brokenStrings) {
+        if (each.length > longestString.length) longestString = each
+      }
+    } else {
+      longestString = text.toString()
     }
+    paint.getTextBounds(longestString, 0, longestString.length, bound)
+    val contentWidth = bound.width()
+    return left + width / 2 - contentWidth / 2
+  }
 
-    override fun getContentTop(): Int {
-        val metrics = paint.fontMetrics
-        val contentHeight = metrics.bottom - metrics.top
-        return (height / 2 - contentHeight / 2).toInt()
-    }
+  override fun getContentTop(): Int {
+    val metrics = paint.fontMetrics
+    val contentHeight = metrics.bottom - metrics.top
+    return (height / 2 - contentHeight / 2).toInt()
+  }
 
-    override fun getContentRight(): Int {
-        val bound = Rect()
-        var longestString = ""
-        if (text.toString().contains("\n")) {
-            val brokenStrings = text.toString().split("\\n").toTypedArray()
-            for (each in brokenStrings) {
-                if (each.length > longestString.length) longestString = each
-            }
-        } else {
-            longestString = text.toString()
-        }
-        paint.getTextBounds(longestString, 0, longestString.length, bound)
-        val contentWidth = bound.width()
-        return left + width / 2 + contentWidth / 2
+  override fun getContentRight(): Int {
+    val bound = Rect()
+    var longestString = ""
+    if (text.toString().contains("\n")) {
+      val brokenStrings = text.toString().split("\\n").toTypedArray()
+      for (each in brokenStrings) {
+        if (each.length > longestString.length) longestString = each
+      }
+    } else {
+      longestString = text.toString()
     }
+    paint.getTextBounds(longestString, 0, longestString.length, bound)
+    val contentWidth = bound.width()
+    return left + width / 2 + contentWidth / 2
+  }
 
-    override fun getContentBottom(): Int {
-        val metrics = paint.fontMetrics
-        val contentHeight = metrics.bottom - metrics.top
-        return (height / 2 + contentHeight / 2).toInt()
-    }
+  override fun getContentBottom(): Int {
+    val metrics = paint.fontMetrics
+    val contentHeight = metrics.bottom - metrics.top
+    return (height / 2 + contentHeight / 2).toInt()
+  }
+
+  private val Int.dp2px: Int
+    get() = TypedValue.applyDimension(
+      TypedValue.COMPLEX_UNIT_DIP, this * 1.0f, Resources.getSystem().displayMetrics
+    ).toInt()
 }
